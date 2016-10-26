952d5494f7b9881421a6ac5c9c79b914ae0819c1
Ivo Valcic <valcicivo@gmail.com>
2016-10-26 20:41:50 +0200

    New view

---
+++ test/AnotherTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnotherTableViewController.h
+//  test
+//
+//  Created by Ivo Valcic on 10/26/16.
+//  Copyright © 2016 Ivo Valcic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnotherTableViewController : UITableViewController
+
+@end
